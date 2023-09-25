bf7d2e819572e005c97a78ae3ccbef09e07f135b
DiegoDiazMtz <diegodiaz220308@gmail.com>
2023-09-25 17:47:41 -0600

    Se establecieron rutas

--- practica3/resources/views/formulario.blade.php
+++ practica3/resources/views/formulario.blade.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Home</title>
+    <title>Formulario</title>
 
 </head>
 <body>
     
-    <h1 class="display-1 text-center text-danger"> HOME </h1>
+    <h1> FORMULARIO </h1>
 
 </body>
 </html>
--- practica3/resources/views/recuerdos.blade.php
+++ practica3/resources/views/recuerdos.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>Home</title>
+    
+    <title>Recuerdos</title>
 
 </head>
 <body>
     
-    <h1 class="display-1 text-center text-danger"> HOME </h1>
+    <h1> RECUERDOS </h1>
 
 </body>
 </html>
