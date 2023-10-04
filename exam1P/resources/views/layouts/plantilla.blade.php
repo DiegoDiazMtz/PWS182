0c0381831b1dbe728b95b69fa6566dcbc5230ffe
DiegoDiazMtz <diegodiaz220308@gmail.com>
2023-10-04 16:32:38 -0600

    Avance1: Scrollspy, card, rutas, plantilla

---
+++ exam1P/resources/views/layouts/plantilla.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.plantilla')
+
+@section('titulo','vistas')
+
+@section('contenido')
+
+    <h1 class="display-1 text-center text-danger mt-5"> Vistas </h1>
+
+@endsection
---
+++ exam1P/resources/views/partials/scrollspy.blade.php
@@ -0,0 +1,31 @@
+<div class="row">
+  <div class="col-4">
+    <div id="list-example" class="list-group">
+      <a class="list-group-item list-group-item-action" href="/">Inicio</a>
+      <a class="list-group-item list-group-item-action" href="/vista1">vista1</a>
+      <a class="list-group-item list-group-item-action" href="/vista2">vista2</a>
+      <a class="list-group-item list-group-item-action" href="/vista3">vista3</a>
+    </div>
+  </div>
+  <div class="col-8">
+    <div data-bs-spy="scroll" data-bs-target="#list-example" data-bs-smooth-scroll="true" class="scrollspy-example" tabindex="0">
+      <h4 id="list-item-1">INICIO</h4>
+      <div class="card" style="width: 18rem;">
+          <div class="card-header">
+            INICIO
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">VISTA 1</li>
+            <li class="list-group-item">VISTA 2</li>
+            <li class="list-group-item">VISTA 3</li>
+          </ul>
+        </div>
+      <h4 id="list-item-2">VISTA 2</h4>
+      <p>...</p>
+      <h4 id="list-item-3">VISTA 3</h4>
+      <p>...</p>
+      <h4 id="list-item-4">VISTA 4</h4>
+      <p>...</p>
+    </div>
+  </div>
+</div>
